8285c987b80c79b1aa187a4e0b9937d768a7e625
Adriana Echeverri <adriana.er98@hotmail.com>
2021-04-28 13:37:48 -0500

    Applied .sort out of variable

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    names = dir(hidden_4).sort()
+    names = dir(hidden_4)
+    names.sort()
     for name in names:
         if (name[:2] == "__"):
             continue
