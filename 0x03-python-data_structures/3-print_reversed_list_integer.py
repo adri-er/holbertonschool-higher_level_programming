9b061e6d6ac9af772a22ee47c1da87183d7d59e3
Adriana Echeverri <adriana.er98@hotmail.com>
2021-05-05 20:17:28 -0500

    Add case empty or not valid

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,6 +3,7 @@
 
 def print_reversed_list_integer(my_list=[]):
 
-    my_list.reverse()
-    for i in my_list:
-        print("{:d}".format(i))
+    if my_list:
+        my_list.reverse()
+        for i in my_list:
+            print("{:d}".format(i))
