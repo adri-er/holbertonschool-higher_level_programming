56773347e363471851be3168ebc5ab5a5449ef36
Adriana Echeverri <adriana.er98@hotmail.com>
2021-05-06 19:23:55 -0500

    Verify if variable is a string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,7 +7,7 @@ def roman_to_int(roman_string):
     roman_num = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500}
     roman_num["M"] = 1000
 
-    if roman_string:
+    if roman_string and type(roman_string) == str:
         for i in range(len(roman_string)):
             if i < len(roman_string) - 1:
                 if roman_num[roman_string[i]] >= roman_num[roman_string[i+1]]:
