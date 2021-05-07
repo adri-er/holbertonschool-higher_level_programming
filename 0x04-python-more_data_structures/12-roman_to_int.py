0ba28434def7dbb958e6b9500b4e662bec1940af
Adriana Echeverri <adriana.er98@hotmail.com>
2021-05-06 19:12:16 -0500

    Avoid for if string is not acceptable

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,13 +7,14 @@ def roman_to_int(roman_string):
     roman_num = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500}
     roman_num["M"] = 1000
 
-    for i in range(len(roman_string)):
-        if i < len(roman_string) - 1:
-            if roman_num[roman_string[i]] >= roman_num[roman_string[i + 1]]:
-                decimal += roman_num[roman_string[i]]
+    if roman_string:
+        for i in range(len(roman_string)):
+            if i < len(roman_string) - 1:
+                if roman_num[roman_string[i]] >= roman_num[roman_string[i+1]]:
+                    decimal += roman_num[roman_string[i]]
+                else:
+                    decimal -= roman_num[roman_string[i]]
             else:
-                decimal -= roman_num[roman_string[i]]
-        else:
-            decimal += roman_num[roman_string[i]]
+                decimal += roman_num[roman_string[i]]
 
     return decimal
