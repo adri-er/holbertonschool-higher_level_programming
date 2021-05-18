9d01cc7ac28b709113c4822ecee1dbaa704b24e1
Adriana Echeverri <adriana.er98@hotmail.com>
2021-05-17 21:46:55 -0500

    New files considering exceptions with err msgs

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+
+    try:
+        result = fct(*args)
+        return result
+    except Exception as err:
+        sys.stderr.write("Exception: {}\n".format(str(err)))
+        return None
