f38a76c43884da35d1945a0d765e40f6ae85d750
Adriana Echeverri <adriana.er98@hotmail.com>
2021-05-18 18:47:48 -0500

    New file to create an empty class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module defines a class of type Square that is empty.
+"""
+
+
+class Square:
+    """ Class that defines a square, in this case it doesn't have attributes.
+
+    Attributes:
+        None.
+
+    """
+    pass
