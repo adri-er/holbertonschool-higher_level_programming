1429284e6be8412564b814770bde017b04b075cd
Adriana Echeverri <adriana.er98@hotmail.com>
2021-05-18 18:59:33 -0500

    New file to create Square class with private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module defines a class Square based on a previous document.
+"""
+
+
+class Square:
+    """The Square class defines a square.
+
+    Attributes:
+       None.
+    """
+    def __init__(self, size):
+        """ Inititialization of the attributes the Square class has.
+        Args:
+           size (int): Size of the square, private attribute.
+
+        """
+        self.__size = size
