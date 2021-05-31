db2c1401b4a224126dca7d01905ae2840646892f
Adriana Echeverri <adriana.er98@hotmail.com>
2021-05-31 18:28:52 -0500

    Documentation specified

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-import json
 """
 In this module a single function that returns JSON of an object.
 """
+import json
 
 
 def to_json_string(my_obj):
-    """ Returns the JSON representation of
-    an object (string). Serializes.
+	""" Returns the JSON representation of
+	an object (string). Serializes.
 
-    Args:
-        my_obj (str): String to serialize.
+	Args:
+		my_obj (str): String to serialize.
 
-    """
-    return json.dumps(my_obj)
+	"""
+	return json.dumps(my_obj)
