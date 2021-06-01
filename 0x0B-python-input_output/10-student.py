a66daf1d32d9a1f1f2e22d55d0c99f73d6027482
Adriana Echeverri <adriana.er98@hotmail.com>
2021-05-31 22:35:34 -0500

    Documentation added

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -14,12 +14,13 @@ class Student:
         age (int): Age of the student.
     """
     def __init__(self, first_name, last_name, age):
+        """ Initializes the class. """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-
+        """ Returns dictionary of the class attributes. """
         if attrs is None:
             return self.__dict__
 
--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -14,9 +14,11 @@ class Student:
         age (int): Age of the student.
     """
     def __init__(self, first_name, last_name, age):
+        """ Initializes the class """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """ Returns dictionary of the class attributes. """
         return self.__dict__
