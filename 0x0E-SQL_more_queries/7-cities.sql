f4126cfe71c163bc3c47161919edf1d91497b075
Adriana Echeverri <adriana.er98@hotmail.com>
2021-07-13 23:51:58 +0000

    [FIX] (rearrange to correct syntaxis of foreign key)

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,5 +2,6 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
        id INT AUTO_INCREMENT PRIMARY KEY,
-       state_id INT FOREIGN KEY REFERENCES states(id),
-       name VARCHAR(256) NOT NULL);
+       name VARCHAR(256) NOT NULL,
+       state_id INT NOT NULL,
+       FOREIGN KEY (state_id) REFERENCES states(id));
