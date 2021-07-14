83352953d50c9c10c5603575a64a99e9109994a6
Adriana Echeverri <adriana.er98@hotmail.com>
2021-07-14 04:10:56 +0000

    [FEATURE]  lists all genres of the show Dexter()

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- lists all genres of the show Dexter.
+SELECT tg.name
+FROM tv_shows AS ts
+INNER JOIN tv_show_genres AS tsg
+ON ts.id = tsg.show_id
+INNER JOIN tv_genres AS tg
+ON tsg.genre_id = tg.id
+WHERE ts.name = 'DEXTER'
+ORDER BY tg.name ASC;
