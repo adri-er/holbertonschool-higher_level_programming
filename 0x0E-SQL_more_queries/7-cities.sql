445fa9bb37955942b6b0b95ec1910878606fbb36
Adriana Echeverri <adriana.er98@hotmail.com>
2021-07-14 04:39:29 +0000

    [REFACTOR] (Refactorize to organize database calls)

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,9 @@
 -- creates the database hbtn_0d_usa and the table cities.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-       id INT AUTO_INCREMENT NOT NULL,
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+       id INT UNIQUE NOT NULL AUTO_INCREMENT,
        name VARCHAR(256) NOT NULL,
        state_id INT NOT NULL,
        PRIMARY KEY (id),
-       FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id));
+       FOREIGN KEY (state_id) REFERENCES states(id));
