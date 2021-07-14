2283784e84836e09a6fe662de76ce82f5233b95d
Adriana Echeverri <adriana.er98@hotmail.com>
2021-07-14 02:29:03 +0000

    [FEATURE] (lists all genres from hbtn_0d_tvshows)

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+SELECT tg.name AS genre, COUNT(tsg.genre_id) AS number_of_shows
+FROM tv_shows_genres AS tsg
+INNER JOIN tv_genres AS tg
+ON tsg.genre_id = tg.id
+GROUP BY tg.name;
