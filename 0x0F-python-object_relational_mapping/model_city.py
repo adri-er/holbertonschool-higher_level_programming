79d0142514b650944f8af1ce67f53696e7f7a4f2
Adriana Echeverri <adriana.er98@hotmail.com>
2021-08-07 20:58:13 -0500

    [FIX] (remove engine from base declarations)

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """ This module defines Base Class for a City and makes
 it a declarative base. """
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sys import argv
 
-engine = create_engine("mysql://{}:{}@localhost" +
-                       ":3306/{}".format(argv[1], argv[2], argv[3]))
+
 Base = declarative_base()
 
 
