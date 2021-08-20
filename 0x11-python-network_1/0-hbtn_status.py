5d1d5cdb55d47737c2c21c78b8e133548e9e3046
adri-er <adriana.er98@hotmail.com>
2021-08-20 18:37:48 +0000

    [FIX] (Rearrange spaces)

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as request:
         information = request.read()
         print("Body response:")
-        print("\t- type: ", type(information))
-        print("\t- content: ", information)
-        print("\t- utf8 content: ", str(information)[2:-1])
+        print("\t- type:", type(information))
+        print("\t- content:", information)
+        print("\t- utf8 content:", str(information)[2:-1])
