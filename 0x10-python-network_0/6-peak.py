2d7079a6dc7faf639739f5e9db47d22ecebb980f
adri-er <adriana.er98@hotmail.com>
2021-08-20 12:44:19 +0000

    [REFACTOR] (Find peak with max value)

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,18 +4,7 @@
 
 def find_peak(list_of_integers):
     """ Finds a peak in a list of integers """
-    answer = None
-    i = 0
-    length = len(list_of_integers)
-
-    while (i < length - 1):
-        if i + 1 < length - 1:
-            if list_of_integers[i + 1] <= list_of_integers[i] and i - 1 < 0:
-                answer = list_of_integers[i]
-                break
-            if i - 1 > 0:
-                if list_of_integers[i + 1] <= list_of_integers[i] and list_of_integers[i - 1] <= list_of_integers[i]:
-                    answer = list_of_integers[i]
-                    break
-        i += 1
-        return answer
+    if len(list_of_integers) > 0:
+        return max(list_of_integers)
+    else:
+        return None
