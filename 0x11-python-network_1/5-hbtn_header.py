b3ff280aa4540655e697161d68703cf89e4a1a78
adri-er <adriana.er98@hotmail.com>
2021-08-20 19:22:47 +0000

    [FEATURE] (Script that works with argv)

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ This module contains a script to fetch a URL """
 import requests
-
+import sys
 
 if __name__ == "__main__":
-    r = requests.get('https://intranet.hbtn.io/status')
+    r = requests.get(sys.argv[1])
     print(r.headers.get('X-Request-Id'))
