6fc8ba65737a480e8289ebdae6dbb886cd3d2d9a
adri-er <adriana.er98@hotmail.com>
2021-08-20 17:43:20 +0000

    [FIX] (Select information of interest)

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 """ This module contains a script to fetch a URL """
 import urllib.request
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as request:
-    print ("Body response:")
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as request:
+        information = request.read()
+        print("Body response:")
+        print("\t- type: ", type(information))
+        print("\t- content: ", information)
+        print("\t- utf8 content: ", str(information)[2:-1])
