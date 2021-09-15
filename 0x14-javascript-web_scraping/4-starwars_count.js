26ba9d1734d1df9f720eee37fbbcd7e307580c29
Adriana Echeverri <adriana.er98@hotmail.com>
2021-09-14 21:17:14 -0500

    [FEATURE] (Script to determine # films with Wedge)

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const process = require('process');
+const request = require('request');
+const requestURL = String(process.argv[2]).slice(0, -5) + 'people/18/';
+console.log(requestURL);
+request(requestURL, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).films.length);
+  }
+});
