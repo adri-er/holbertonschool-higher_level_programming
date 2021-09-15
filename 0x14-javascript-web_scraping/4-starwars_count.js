78e70cf2ab0cdda419b0544707bd6c8d8efae8eb
Adriana Echeverri <adriana.er98@hotmail.com>
2021-09-14 21:44:47 -0500

    [REFACTOR] (Use cycle instead of changing url)

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,20 @@
 #!/usr/bin/node
 const process = require('process');
 const request = require('request');
-const requestURL = String(process.argv[2]).slice(0, -5) + 'people/18/';
+const requestURL = String(process.argv[2]);
+let amount = 0;
 
 request(requestURL, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    console.log(JSON.parse(body).films.length);
+    const jsonBodyFilms = JSON.parse(body).results;
+    const numberFilms = JSON.parse(body).count;
+    for (let index = 0; index < numberFilms; index++) {
+      if (jsonBodyFilms[index].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+        amount = amount + 1;
+      }
+    }
+    console.log(amount);
   }
 });
