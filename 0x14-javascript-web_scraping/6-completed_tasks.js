78ff807e722797bf5f523fb10a43f1aa7bf04252
Adriana Echeverri <adriana.er98@hotmail.com>
2021-09-14 23:20:42 -0500

    [STYLE] (Semistandard)

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -8,7 +8,7 @@ request(requestURL, function (error, response, body) {
     console.log(error);
   } else {
     const listTodo = JSON.parse(body);
-    const users = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0};
+    const users = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0 };
     for (let index = 0; index < listTodo.length; index++) {
       if (listTodo[index].completed === true) {
         users[listTodo[index].userId] += 1;
