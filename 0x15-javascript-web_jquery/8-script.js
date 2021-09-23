2ea365b16661e8fc9c5d0acde6af734d56b1f791
Adriana Echeverri <adriana.er98@hotmail.com>
2021-09-22 20:53:41 -0500

    [STYLE] (Semistandard)

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,9 +1,9 @@
-$(function (){
+$(function () {
   $.ajax({
     type: 'GET',
     url: 'https://swapi-api.hbtn.io/api/films/?format=json',
-    success: function(response) {
-      $.each(response.results, function(i, item) {
+    success: function (response) {
+      $.each(response.results, function (i, item) {
         $('UL#list_movies').append('<li>' + item.title + '</li>');
       });
     }
